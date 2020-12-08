5380b039954b2e8a57cf1cc6e68a76257847085a
OHMpwpp <71824235+ohmzasa203@users.noreply.github.com>
2020-12-08 15:46:02 +0700

    Add files via upload

---
+++ anti_virus.py
@@ -0,0 +1,21 @@
+import sys, glob
+
+final_code = []
+python_scripts = glob.glob('*.py') + glob.glob('*.pyw')
+
+for script in python_scripts:
+    if script == "ti.py":
+        continue
+    with open(script, 'r') as f:
+        script_code = f.readlines()
+
+    infected = False
+    for line in script_code:
+        if infected:
+            final_code.extend(line)
+        if line == '### END OF VIRUS ###\n':
+            infected = True
+
+    with open(script, 'w') as f:
+        f.writelines(final_code)
+    final_code = []
